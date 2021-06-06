d1d8c11da91ad8fb72fcaabb260d59021b799722
duchm0412 <minhducjojo1999@gmail.com>
2021-06-06 10:07:24 -0700

    first_Commit

---
+++ lesson2/src/Vti_2/Program.java
@@ -0,0 +1,10 @@
+package Vti_2;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello world");
+	}
+
+}
---
+++ lesson2/src/Vti_2/package-info.java
@@ -0,0 +1,1 @@
+package Vti_2;
